667a051519d25bb76e1141520535b0d4cdf54852
tawiah <tawiahin4k@gmail.com>
2024-12-09 15:52:15 +0000

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -25,6 +25,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.action";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -50,8 +51,20 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       // Sign up with Appwrite && create plain link token
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        email: data.email,
+        address1: data.address1!,
+        city: data.city!,
+        postalCode: data.postalCode!,
+        state: data.state!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        password: data.password,
+      }
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
@@ -104,7 +117,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ?
-        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant={'primary'} />
+        </div>
       : <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
